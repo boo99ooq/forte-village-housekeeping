703c50dd38c7bfde9bf43df31f345f4b61379576
boo99ooq <giusmasala99@gmail.com>
2026-01-11 19:12:47 +0100

    Update fv-lab1.py

--- fv-lab1.py
+++ fv-lab1.py
@@ -284,26 +284,31 @@ with t_plan:
         pool_spl = attive[attive['Ruolo'] == 'Cameriera'].head(4)['Nome'].tolist()
         st.session_state['spl_v_fin'] = pool_spl
         
+       # 1. Pulizia nomi colonne
         conf_df.columns = conf_df.columns.str.strip().str.upper()
+
+        # 2. Calcolo Fabbisogni
         fabb = {}
         for h in lista_hotel:
             m = conf_df[conf_df['HOTEL'] == h]
             if not m.empty:
-                m_ai = m.iloc[0]['ARR I']
-                m_fi = m.iloc[0]['FERM I']
-                m_ag = m.iloc[0]['ARR G']
-                m_fg = m.iloc[0]['FERM G']
+                m_ai, m_fi = m.iloc[0]['ARR I'], m.iloc[0]['FERM I']
+                m_ag, m_fg = m.iloc[0]['ARR G'], m.iloc[0]['FERM G']
             else:
                 m_ai, m_fi, m_ag, m_fg = 60, 30, 45, 25
             
             tot_fer = cur_inp[h]["FI"] + cur_inp[h]["FG"]
             fabb[h] = (cur_inp[h]["AI"]*m_ai + cur_inp[h]["FI"]*m_fi + cur_inp[h]["AG"]*m_ag + cur_inp[h]["FG"]*m_fg + tot_fer*15) / 60
         
+        # 3. Definizione Macro e Ordine Zone
         fabb["MACRO: PALME & GARDEN"] = fabb.get("Le Palme", 0) + fabb.get("Hotel Castello Garden", 0)
         z_ord = ["Hotel Castello", "Hotel Castello 4 Piano", "MACRO: PALME & GARDEN"] + [h for h in lista_hotel if h not in ["Hotel Castello", "Hotel Castello 4 Piano", "Le Palme", "Hotel Castello Garden"]]
         
+        # 4. Inizio Assegnazione
         gia_a, ris = set(), []
-        for zona in z_ord:        
+        for zona in z_ord:
+            # Qui continuerà la tua logica (es: cameriere_disponibili = ...)
+            pass  
         # Logica di assegnazione
         fabb["MACRO: PALME & GARDEN"] = fabb.get("Le Palme", 0) + fabb.get("Hotel Castello Garden", 0)
         z_ord = ["Hotel Castello", "Hotel Castello 4 Piano", "MACRO: PALME & GARDEN"] + [h for h in lista_hotel if h not in ["Hotel Castello", "Hotel Castello 4 Piano", "Le Palme", "Hotel Castello Garden"]]
