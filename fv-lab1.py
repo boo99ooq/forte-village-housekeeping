99fee3c62d37d2469eb4b5365c252c98eb6c60b3
boo99ooq <giusmasala99@gmail.com>
2026-01-11 19:33:54 +0100

    Update fv-lab1.py

--- fv-lab1.py
+++ fv-lab1.py
@@ -219,7 +219,7 @@ with t_plan:
             "FG": r[4].number_input("FG", 0, 100, 0, key=f"p_fg_{h}", label_visibility="collapsed")
         }
 
-   if st.button("🚀 GENERA SCHIERAMENTO", use_container_width=True):
+    if st.button("🚀 GENERA SCHIERAMENTO", use_container_width=True):
         conf_df = pd.read_csv(FILE_CONFIG) if os.path.exists(FILE_CONFIG) else pd.DataFrame()
         attive = df[~df['Nome'].isin(assenti)].copy()
         
